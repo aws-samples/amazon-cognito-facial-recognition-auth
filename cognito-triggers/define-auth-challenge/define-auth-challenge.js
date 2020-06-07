e4369262c39823e961777ecfbe38309048c88da5
Enrico Bergamo <enrico_bergamo_91@hotmail.com>
2020-06-07 17:16:55 -0300

    Explicit denial of non-custom auth

--- cognito-triggers/define-auth-challenge/define-auth-challenge.js
+++ cognito-triggers/define-auth-challenge/define-auth-challenge.js
@@ -4,6 +4,11 @@ exports.handler = async (event, context) => {
     console.log("Define Auth Challenge: " + JSON.stringify(event));
 
     if (event.request.session &&
+        event.request.session.find(attempt => attempt.challengeName !== 'CUSTOM_CHALLENGE')) {
+        // We only accept custom challenges; fail auth
+        event.response.issueTokens = false;
+        event.response.failAuthentication = true;
+    } else if (event.request.session &&
         event.request.session.length >= 3 &&
         event.request.session.slice(-1)[0].challengeResult === false) {
         // The user provided a wrong answer 3 times; fail auth
@@ -11,6 +16,7 @@ exports.handler = async (event, context) => {
         event.response.failAuthentication = true;
     } else if (event.request.session &&
         event.request.session.length &&
+        event.request.session.slice(-1)[0].challengeName === 'CUSTOM_CHALLENGE' &&
         event.request.session.slice(-1)[0].challengeResult === true) {
         // The user provided the right answer; succeed auth
         event.response.issueTokens = true;
